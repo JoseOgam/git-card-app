0050789833c214f54a316d7ed46f4f9a29fb034a
JoseOgam <jotienoochieng@gmail.com>
2020-07-21 18:07:49 -0400

    woking with promise, axios to get data from github api

--- src/app.js
+++ src/app.js
@@ -1,58 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-const gitApi = [
-  {
-    avatar_url: "https://avatars3.githubusercontent.com/u/44178450?v=4",
-    company: "Scholar",
-    name: "JoseOgam",
-  },
-  {
-    avatar_url: "https://avatars2.githubusercontent.com/u/140611?v=4",
-    company: "facebook",
-    name: "Rafael Villar Burke (Pachi)",
-  },
-  {
-    avatar_url: "https://avatars2.githubusercontent.com/u/3344757?v=4",
-    company: "Twitter",
-    name: "oTTa",
-  },
-];
+// const gitApi = [
+//   {
+//     avatar_url: "https://avatars3.githubusercontent.com/u/44178450?v=4",
+//     company: "Scholar",
+//     name: "JoseOgam",
+//   },
+//   {
+//     avatar_url: "https://avatars2.githubusercontent.com/u/140611?v=4",
+//     company: "facebook",
+//     name: "Rafael Villar Burke (Pachi)",
+//   },
+//   {
+//     avatar_url: "https://avatars2.githubusercontent.com/u/3344757?v=4",
+//     company: "Twitter",
+//     name: "oTTa",
+//   },
+// ];
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: [],
+    };
+  }
+
   render() {
+    var addProfile = (profileData) => {
+      this.setState((prevState) => ({
+        profiles: [...prevState.profiles, profileData],
+      }));
+    };
     return (
       <div>
-        <div className="header">
-          <h1 className="header-title">Github Card App</h1>
-        </div>
+        <h1 className="header">Github Card Title</h1>
         <div className="container">
-          <Form />
-          <CardList />
+          <Form onSubmit={addProfile} />
+          <CardList profiles={this.state.profiles} />
         </div>
       </div>
     );
   }
 }
-const CardList = () => {
+const CardList = (props) => {
   return (
     <div>
-      {gitApi.map((profile) => (
-        <Card key={profile.name} {...profile} />
+      {props.profiles.map((profile) => (
+        <Card key={profile.id} {...profile} />
       ))}
     </div>
   );
 };
+
 class Card extends React.Component {
   render() {
     const profile = this.props;
     return (
-      <div className="card">
-        <img className="avatar" src={profile.avatar_url} />
-        <p>Name:- {profile.name}</p>
-        <p>company:- {profile.company}</p>
+      <div>
+        <img className="avatar" src={profile.avatar_url} alt="profile" />
+        <p>{profile.name}</p>
+        <p>{profile.company}</p>
       </div>
     );
   }
@@ -66,27 +78,28 @@ class Form extends React.Component {
     };
   }
   render() {
-    // var userNameInput = React.createRef();
-    var handleSubmit = (e) => {
+    // var inputUserName = React.createRef();
+    var handleSubmit = async (e) => {
       e.preventDefault();
-      const resp = axios.get(
-        `https://api.github.com/users/${his.state.userName}`
+      const resp = await axios.get(
+        `https://api.github.com/users/${this.state.userName}`
       );
-      console.log(resp);
+      this.props.onSubmit(resp.data);
+      this.setState({ userName: "" });
     };
     return (
       <div>
-        <form onSubmit={handleSubmit} className="add-user">
-          <input
-            type="text"
-            className="form-input"
-            placeholder="Github Username"
-            // ref={userNameInput}
-            value={this.state.userName}
-            onChange={(e) => this.setState({ userName: e.target.value })}
-            required
-          />
-          <button>add user</button>
+        <form>
+          {
+            <input
+              placeholder="enter github username"
+              required
+              type="text"
+              value={this.state.userName}
+              onChange={(e) => this.setState({ userName: e.target.value })}
+            />
+          }
+          <button onClick={handleSubmit}>add card</button>
         </form>
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require("path");
 module.exports = {
-  entry: "./src/app.js",
+  entry: ["babel-polyfill", "./src/app.js"],
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js",
