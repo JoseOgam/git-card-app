7a2992321ac034a6e1dc82048a75032d1fe82263
JoseOgam <jotienoochieng@gmail.com>
2020-07-22 07:39:44 -0400

    config for deploying to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,48 @@
 var path = require("path");
-module.exports = {
-  entry: ["babel-polyfill", "./src/app.js"],
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js*$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-  },
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = (env) => {
+  var isProduction = env === "production";
+  var CSSExtract = new ExtractTextPlugin("gitcard.css");
+  return {
+    entry: ["babel-polyfill", "./src/app.js"],
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js*$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+          // use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+    },
+  };
 };
